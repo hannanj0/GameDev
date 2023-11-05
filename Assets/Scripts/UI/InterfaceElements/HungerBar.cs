f60f410a6af02b7e35e441d37a2b75478a411781
Hannan <94841087+hannanj0@users.noreply.github.com>
2023-11-05 14:12:22 +0000

    CommentsAdded
    To necessary scripts

--- Assets/Scripts/UI/InterfaceElements/HungerBar.cs
+++ Assets/Scripts/UI/InterfaceElements/HungerBar.cs
@@ -4,21 +4,30 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
+/// <summary>
+/// The HungerBar script is to showcase the player's hunger throughout the game, which changes value depending on the type of interactions taken
+/// </summary>
 public class HungerBar : MonoBehaviour
 {
-    private Slider slider;
-    public TextMeshProUGUI hungerCount;
+    private Slider slider; // This is a reference to the UI slider
+    public TextMeshProUGUI hungerCount; // This is a reference to the TMP text which displays the hunger count
 
-    public GameObject playerState;
-    public GameObject playerMovement; // Reference to the PlayerMovement script
+    public GameObject playerState; // This is a reference to the player's state script
+    public GameObject playerMovement; // This is a reference to the player's movement script
 
-    private float currentHunger, maxHunger;
+    private float currentHunger, maxHunger; // Variables to store the current and max hunger values
 
+    /// <summary>
+    /// This initialises the slider by getting it from the GameObject
+    /// </summary>
     void Start()
     {
         slider = GetComponent<Slider>();
     }
-
+    
+    /// <summary>
+    /// This will determine what the fill value of the Hunger Bar slider should be, as well as ensuring the Player can sprint only if the current hunger is greater than 0
+    /// </summary>
     void Update()
     {
         currentHunger = playerState.GetComponent<PlayerState>().currentHunger;
@@ -29,8 +38,8 @@ public class HungerBar : MonoBehaviour
 
         hungerCount.text = currentHunger + "/" + maxHunger;
 
-        // Check if the player's hunger has reached 0 and update sprinting in PlayerMovement
-        bool canSprint = currentHunger > 0; // Allow sprinting if hunger is above 0
+        
+        bool canSprint = currentHunger > 0;
         playerMovement.GetComponent<PlayerMovement>().UpdateSprinting(canSprint);
     }
 }
