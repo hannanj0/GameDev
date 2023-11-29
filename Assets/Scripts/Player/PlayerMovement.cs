ca9491fc66641015337ddb159a1ddc2106dfe3cd
Hugo Lapa <Hugolapa888@gmail.com>
2023-11-29 17:30:18 +0000

    Jumping control + animation

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -10,22 +10,27 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 moveInput;
     private Vector3 moveDirection;
     private bool isRunning = false;
-    private bool canSprint = true;
+    private bool canSprint = true; // This needs to be kept if UpdateSprinting is used
+    private bool isJumping = false;
     private Rigidbody rb;
     private PlayerControls controls;
-    private Animator animator; // Added Animator variable
+    private Animator animator;
+
+    public float jumpForce = 7.0f;
+    private float groundCheckDistance = 0.2f;
+    public LayerMask groundLayer;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
-        animator = GetComponent<Animator>(); // Initialize the Animator variable
+        animator = GetComponent<Animator>();
         controls = new PlayerControls();
 
-        // Subscribe to the input system events
         controls.Gameplay.Move.performed += OnMovePerformed;
         controls.Gameplay.Move.canceled += OnMoveCanceled;
         controls.Gameplay.Sprint.performed += OnSprintPerformed;
         controls.Gameplay.Sprint.canceled += OnSprintCanceled;
+        controls.Gameplay.Jump.performed += OnJumpPerformed;
     }
 
     void OnEnable()
@@ -41,13 +46,73 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         UpdateMoveDirection();
-        UpdateAnimation(); // Call UpdateAnimation method
+        UpdateAnimation();
     }
 
     private void FixedUpdate()
     {
+
         UpdatePosition();
+
+        if (isJumping)
+        {
+            if (IsGrounded())
+            {
+                rb.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
+                isJumping = false;
+            }
+        }
+        else
+        {
+            if (IsGrounded() && animator.GetBool("isJumping"))
+            {
+                animator.SetBool("isJumping", false);
+            }
+        }
     }
+
+    private void OnJumpPerformed(InputAction.CallbackContext context)
+    {
+        Debug.Log("Jump input received"); // Check if the jump input is detected
+        Jump();
+    }
+
+    private void Jump()
+    {
+        if (IsGrounded())
+        {
+            Debug.Log("Attempting to jump"); // Confirm the jump is attempted
+            animator.SetBool("isJumping", true);
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange); // Apply the jump force
+            isJumping = true;
+        }
+        else
+        {
+            Debug.Log("Player tried to jump but wasn't grounded"); // Check if the player is grounded
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        // The origin is now at the bottom of the collider, not the center of the GameObject.
+        Vector3 origin = transform.position + (Vector3.up * 0.1f);
+        float checkDistance = groundCheckDistance + 0.1f; // You might need to adjust this value.
+
+        // Visualize the raycast in the scene view.
+        Debug.DrawRay(origin, Vector3.down * checkDistance, Color.red);
+
+        RaycastHit hit;
+        if (Physics.Raycast(origin, Vector3.down, out hit, checkDistance, groundLayer))
+        {
+            Debug.Log($"Hit: {hit.collider.name}"); // This will tell you what the raycast hit.
+            return true;
+        }
+
+        return false;
+    }
+
+
+
 
     private void OnMovePerformed(InputAction.CallbackContext context)
     {
@@ -61,7 +126,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnSprintPerformed(InputAction.CallbackContext context)
     {
-        isRunning = context.ReadValueAsButton();
+        isRunning = context.ReadValueAsButton() && canSprint;
     }
 
     private void OnSprintCanceled(InputAction.CallbackContext context)
@@ -73,29 +138,36 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector3 directionRelativeToCamera = new Vector3(moveInput.x, 0, moveInput.y).normalized;
         moveDirection = cameraTransform.TransformDirection(directionRelativeToCamera);
-        moveDirection.y = 0; // Ensure that the player remains grounded
+        moveDirection.y = 0;
     }
 
     private void UpdateAnimation()
-    {
-        // Convert global movement direction to local space
-        Vector3 localMoveDirection = transform.InverseTransformDirection(moveDirection);
+{
+    Vector3 localMoveDirection = transform.InverseTransformDirection(moveDirection);
+    animator.SetFloat("MoveX", localMoveDirection.x);
+    animator.SetFloat("MoveZ", localMoveDirection.z);
+    animator.SetBool("isRunning", isRunning);
 
-        // Update the Animator parameters with local direction
-        animator.SetFloat("MoveX", localMoveDirection.x);
-        animator.SetFloat("MoveZ", localMoveDirection.z);
-        animator.SetBool("isRunning", isRunning);
+    // Check if we are falling (y velocity is negative and we are not on the ground)
+    bool falling = rb.velocity.y < 0 && !IsGrounded();
+    animator.SetBool("isFalling", falling);
+
+    // Ensure we are not interrupting the jump up animation
+    if (IsGrounded())
+    {
+        animator.SetBool("isFalling", false); // Reset the isFalling when grounded
     }
+}
 
 
     private void UpdatePosition()
     {
-        isRunning = isRunning && canSprint && moveDirection.magnitude > 0.1f;
         float currentSpeed = isRunning ? speed * runMultiplier : speed;
         Vector3 movement = moveDirection * currentSpeed * Time.fixedDeltaTime;
         rb.MovePosition(rb.position + movement);
     }
 
+    // This method allows external scripts to enable or disable sprinting.
     public void UpdateSprinting(bool sprintAllowed)
     {
         canSprint = sprintAllowed;
