24bf9e200c95b2903be61db131200c5081865066
Hannan <94841087+hannanj0@users.noreply.github.com>
2024-01-05 22:14:46 +0000

    NPC Interaction Fix
    Fixed so that the interaction with the NPC only happens when looking at the NPC and is near them.

--- Assets/Scripts/Interactions/InteractableObject.cs
+++ Assets/Scripts/Interactions/InteractableObject.cs
@@ -1,8 +1,6 @@
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.Timeline;
 using UnityEngine.Playables;
-using UnityEngine.InputSystem; // Include the Input System namespace
 
 namespace StarterAssets.Interactions
 {
@@ -10,30 +8,12 @@ namespace StarterAssets.Interactions
     {
         [SerializeField] private UnityEvent _onInteract;
         [SerializeField] private PlayableDirector cutsceneDirector;
-
-        private PlayerControls controls; // Reference to PlayerControls
         private bool cutscenePlayed = false;
 
         UnityEvent InteractionInterface.onInteract
         {
             get => _onInteract;
             set => _onInteract = value;
-        }
-
-        void Awake()
-        {
-            controls = new PlayerControls(); // Initialize the PlayerControls
-            controls.Gameplay.Interact.performed += context => Interact();
-        }
-
-        void OnEnable()
-        {
-            controls.Gameplay.Interact.Enable(); // Enable the Interact action
-        }
-
-        void OnDisable()
-        {
-            controls.Gameplay.Interact.Disable(); // Disable the Interact action
         }
 
         public void Interact()
