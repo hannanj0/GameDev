10f10bf37fc9b7bfa1e63144f468aecb556dc72f
Hugo Lapa <Hugolapa888@gmail.com>
2024-01-07 23:52:43 +0000

    Dragon boss

---
+++ Assets/Scripts/Enemies/AIBoss.cs
@@ -0,0 +1,121 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIBoss : MonoBehaviour
+{
+    public NavMeshAgent navMeshAgent;
+    public float viewRadius = 75;
+    public float viewAngle = 360;
+    public LayerMask playerMask;
+    public LayerMask obstacleMask;
+    public float speedRun = 9; // Chasing speed of AI
+    public float attackRange = 10f;
+
+
+    private Animator animator;
+    private Vector3 m_PlayerPosition;
+    private bool m_PlayerInRange;
+    private bool m_CaughtPlayer;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent.isStopped = true; // Start with the dragon being idle
+        
+
+        animator = GetComponent<Animator>();
+        if (animator == null)
+        {
+            Debug.LogError("Animator component missing from this GameObject", this);
+        }
+    }
+
+    void Update()
+    {
+        if (!m_CaughtPlayer)
+        {
+            EnvironmentView();
+            if (m_PlayerInRange)
+            {
+                Chasing();
+            }
+            else
+            {
+                Idle();
+            }
+        }
+    }
+
+    private void Chasing()
+    {
+        float distanceToPlayer = Vector3.Distance(transform.position, m_PlayerPosition);
+
+        if (distanceToPlayer <= viewRadius)
+        {
+            // Only move towards the player if they are outside of attack range
+            if (distanceToPlayer > attackRange)
+            {
+                Move(speedRun);
+                navMeshAgent.SetDestination(m_PlayerPosition);
+                animator.SetBool("Walk", true);
+                animator.SetBool("Attack", false);
+            }
+            else
+            {
+                // Stop and attack if within attack range
+                navMeshAgent.isStopped = true;
+                animator.SetBool("Walk", false);
+                animator.SetBool("Attack", true);
+            }
+        }
+        else
+        {
+            Idle();
+        }
+    }
+
+    private void Idle()
+    {
+        navMeshAgent.isStopped = true;
+        if (animator != null)
+        {
+            animator.SetBool("Walk", false);
+            animator.SetBool("Attack", false);
+        }
+    }
+
+
+    private void Move(float speed)
+    {
+        navMeshAgent.isStopped = false;
+        navMeshAgent.speed = speed;
+    }
+
+    void EnvironmentView()
+    {
+        Collider[] playerInRange = Physics.OverlapSphere(transform.position, viewRadius, playerMask);
+        m_PlayerInRange = false;
+        foreach (var playerCollider in playerInRange)
+        {
+            Transform player = playerCollider.transform;
+            Vector3 dirToPlayer = (player.position - transform.position).normalized;
+            if (Vector3.Angle(transform.forward, dirToPlayer) < viewAngle / 2)
+            {
+                float dstToPlayer = Vector3.Distance(transform.position, player.position);
+                if (!Physics.Raycast(transform.position, dirToPlayer, dstToPlayer, obstacleMask))
+                {
+                    m_PlayerInRange = true;
+                    m_PlayerPosition = player.transform.position;
+                }
+            }
+        }
+    }
+
+    public void OnDeath()
+    {
+        if (animator != null)
+        {
+            animator.SetTrigger("Die");
+        }
+    }
+}
